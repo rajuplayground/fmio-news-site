2e7f0f466b6952a535ddf039608a0a5b67a16807
Raju George <admin@goingcloudnative.com>
2023-03-21 13:29:57 +0530

    Menu functionality completed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const burgerMenuBtn = document.getElementById('burger-menu');
+const menuCloseBtn = document.getElementById('menu-close-btn');
+const mobileMenu = document.getElementById('mobile-nav');
+
+burgerMenuBtn.addEventListener('click', (e) => {
+    mobileMenu.dataset.menu = "open"
+})
+
+menuCloseBtn.addEventListener('click', (e) => {
+    mobileMenu.dataset.menu = "close"
+})
